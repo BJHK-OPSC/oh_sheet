fe6cb5f9942e81d52910973fda1526879425448e
mattbosch <mattbosch16@gmail.com>
2023-06-05 22:46:36 +0200

    Landing page buttons redirect to sign in and sign up pages. Sign In and Sign Up page functionality. Sign In page redirects to main_activity.

--- app/src/main/java/com/example/oh_sheet/SignInActivity.kt
+++ app/src/main/java/com/example/oh_sheet/SignInActivity.kt
@@ -1,11 +1,55 @@
 package com.example.oh_sheet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.content.SharedPreferences
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (isValidCredentials(username, password)) {
+                // Start a new activity after successful login
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, LandingPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        val registeredUsername = sharedPreferences.getString("username", "")
+        val registeredPassword = sharedPreferences.getString("password", "")
+
+        return username == registeredUsername && password == registeredPassword
+    }
+
 }
--- app/src/main/java/com/example/oh_sheet/SignUpActivity.kt
+++ app/src/main/java/com/example/oh_sheet/SignUpActivity.kt
@@ -1,11 +1,80 @@
 package com.example.oh_sheet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.app.Activity
+import android.content.SharedPreferences
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var registerUsernameEditText: TextInputEditText
+    private lateinit var registerPasswordEditText: EditText
+    private lateinit var registerConfirmPasswordEditText: EditText
+    private lateinit var registerButton: Button
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        registerUsernameEditText = findViewById(R.id.usernameEditText)
+        registerPasswordEditText = findViewById(R.id.editTextTextPassword)
+        registerConfirmPasswordEditText = findViewById(R.id.editTextConfirmPassword)
+        registerButton = findViewById(R.id.registerButton)
+
+        registerButton.setOnClickListener {
+            val username = registerUsernameEditText.text.toString()
+            val password = registerPasswordEditText.text.toString()
+            val confirmPassword = registerConfirmPasswordEditText.text.toString()
+
+            when (val validationResult = isValidRegistration(username, password, confirmPassword)) {
+                RegistrationValidationResult.SUCCESS -> {
+                    // Pass the registered credentials back to MainActivity
+                    val intent = Intent()
+                    intent.putExtra("username", username)
+                    intent.putExtra("password", password)
+                    setResult(Activity.RESULT_OK, intent)
+                    finish()
+                }
+                RegistrationValidationResult.PASSWORDS_DONT_MATCH -> {
+                    registerConfirmPasswordEditText.error = "Passwords do not match"
+                }
+                RegistrationValidationResult.MISSING_FIELDS -> {
+                    Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, LandingPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+    private fun isValidRegistration(username: String, password: String, confirmPassword: String): RegistrationValidationResult {
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            return RegistrationValidationResult.MISSING_FIELDS
+        }
+
+        if (password != confirmPassword) {
+            return RegistrationValidationResult.PASSWORDS_DONT_MATCH
+        }
+
+        return RegistrationValidationResult.SUCCESS
+    }
+
+    enum class RegistrationValidationResult {
+        SUCCESS,
+        PASSWORDS_DONT_MATCH,
+        MISSING_FIELDS
+    }
+
 }
