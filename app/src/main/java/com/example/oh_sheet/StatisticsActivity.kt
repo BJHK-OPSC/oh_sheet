9d92dbf56f4658a37a6bcb5f624de013a40ec110
mattbosch <mattbosch16@gmail.com>
2023-06-06 16:38:49 +0200

    Added the function for category totals (not working yet but logic is there)

--- app/src/main/java/com/example/oh_sheet/StatisticsActivity.kt
+++ app/src/main/java/com/example/oh_sheet/StatisticsActivity.kt
@@ -2,10 +2,33 @@ package com.example.oh_sheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.time.LocalDate
+import java.time.Duration
 
 class StatisticsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+
+      /*  fun calculateTimeTotals(startDate: LocalDate, endDate: LocalDate): Map<String, Duration> {
+            val timeTotals = mutableMapOf<String, Duration>()
+
+            for (entry in timesheetEntries) {
+                if (entry.date.isAfter(endDate) || entry.date.isBefore(startDate)) {
+                    continue // Skip entries outside the selected date range
+                }
+
+                val duration = Duration.between(entry.startTime, entry.endTime)
+
+                if (timeTotals.containsKey(entry.category)) {
+                    val totalDuration = timeTotals[entry.category]?.plus(duration)
+                    timeTotals[entry.category] = totalDuration
+                } else {
+                    timeTotals[entry.category] = duration
+                }
+            }
+
+            return timeTotals
+        }*/
     }
 }
